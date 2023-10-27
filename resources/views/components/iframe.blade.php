7446bf0b68da5c50b106bcff4e752e703e0f5b31
Dennis_INDIGOKIND <dennis@indigokind.de>
2023-10-27 23:24:53 +0200

    wip

--- resources/views/components/iframe.blade.php
+++ resources/views/components/iframe.blade.php
@@ -1,4 +1,4 @@
 @props([
     'path' => ''
 ])
-<iframe class="mt-12" src="{{ $path }}" style="width: 100vw; height:92%;" frameborder="0"></iframe>
+<iframe class="mt-12" src="{{ $path }}" style="width: 100%; height:92%;" frameborder="0"></iframe>
