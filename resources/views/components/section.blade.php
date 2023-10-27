7f12a2c232bcfd0a1a733be49b099cfb1fd2eb44
Dennis_INDIGOKIND <dennis@indigokind.de>
2023-10-28 00:53:34 +0200

    wip

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -11,7 +11,7 @@
         @if($link)
             <div class="absolute -top-9 @if($link) block @else invisible @endif">
                 <a class="text-white max-w-fit outline" href="{{ route( $link ) }}">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-16 md:w-20 h-16 md:h-20 -mt-14 md:-mt-16 cursor-pointer transform md:hover:scale-105 transition">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-16 md:w-20 h-16 md:h-20 -mt-14 md:-mt-16 cursor-pointer transform hover:scale-105 transition">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                     </svg>
                 </a>
