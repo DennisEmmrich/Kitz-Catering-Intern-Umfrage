343b2a11a44855b1c6819314c63c5e39fc3c72f1
Dennis_INDIGOKIND <dennis@indigokind.de>
2024-01-19 17:06:45 +0100

    changed aspect of x-iframe for pdf

--- resources/views/components/iframe.blade.php
+++ resources/views/components/iframe.blade.php
@@ -1,4 +1,4 @@
 @props([
     'path' => ''
 ])
-<iframe class="mt-12" src="{{ $path }}" style="width: 100%; height:92%;" frameborder="0"></iframe>
+<iframe class="mt-12 aspect-[1/1.414]" src="{{ $path }}" style="width: 100%; height:92%;" frameborder="0"></iframe>
