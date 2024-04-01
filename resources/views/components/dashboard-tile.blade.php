ff3e98070d36b568386f88b223480d352ec17dbf
Dennis_INDIGOKIND <dennis@indigokind.de>
2024-04-01 20:12:59 +0200

    wip

--- resources/views/components/dashboard-tile.blade.php
+++ resources/views/components/dashboard-tile.blade.php
@@ -3,7 +3,7 @@
     'path' => '',
 ])
 <div class="cursor-pointer" draggable="false">
-    <a class="no-link-bgColor" href="{{ $link }}">
+    <a class="no-tap-highlight" href="{{ $link }}">
         <div class="w-full h-56 grid content-end text-white text-2xl font-bold font-sans rounded-xl shadow-md bg-cover bg-center overflow-hidden" style="background-image: url('{{ asset( $path ) }}');">
             <div class="p-3 backdrop-blur-sm bg-bgColorSecondary/30 whitespace-nowrap">
                 {{ $headline }}
